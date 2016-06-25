2257e2cab7ed42549c92754ba5f0d79e6e410129
salman <salmanmasood24@yahoo.com>
2016-06-25 19:44:22 +0500

    1st page done

--- New folder/javascript.js
+++ New folder/javascript.js
@@ -3,8 +3,7 @@ A=new Array("images/s1.jpg","images/s2.jpg","images/s3.png");
 function myfn()
 {	
 
-while(true)
-{
+
 switch(temp) {
 
     case 0: document.getElementById("sliderimg").src = A[0];
@@ -17,6 +16,24 @@ case 2: document.getElementById("sliderimg").src = A[2];
 	 temp = 0
 	 break;
 	 	  
+
 }
+
 }
+
+function myfn2()
+{
+switch(temp) {
+
+    case 0: document.getElementById("sliderimg").src = A[1];
+	temp=2;
+	 break;
+case 1: document.getElementById("sliderimg").src = A[2];
+	 temp = 0;
+	 break;
+case 2: document.getElementById("sliderimg").src = A[0];
+	 temp = 1;
+	 break;
+	 	  
+} 
 }
