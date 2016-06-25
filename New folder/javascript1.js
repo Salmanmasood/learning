e03780b7cf119ac4547fa4a9cfd039bd8d6ff5d2
salman <salmanmasood24@yahoo.com>
2016-06-25 19:29:29 +0500

    2nd page

---
+++ New folder/javascript1.js
@@ -0,0 +1,44 @@
+function bilal()
+{
+document.getElementById("faimg").src="images/p2.jpg";	
+document.getElementById("name").innerHTML="Muhammad Bilal Siddiqui.";	
+document.getElementById("edu").innerHTML="Msc Applied Physics."	;
+document.getElementById("exp").innerHTML="4 years of teaching Maths & Physics.";
+document.getElementById("ext").innerHTML="Physics & English.";		
+document.getElementById("det").innerHTML="Sir Bilal is very elegant and honest person, He does his work with hardwork and fully dedication ,He is countable in the list of best teachers, He has been also the student of University of Karachi ";		
+	
+}
+function ibtihaj()
+{
+document.getElementById("faimg").src="images/p1.jpg";	
+document.getElementById("name").innerHTML="Ibtihaj Khurram.";	
+document.getElementById("edu").innerHTML="Msc Applied Chemistry."	;
+document.getElementById("exp").innerHTML="5years of teaching Maths & Physics.";
+document.getElementById("exp").innerHTML="5years of teaching Maths & Physics.";		
+document.getElementById("ext").innerHTML="Maths,Physics & Chemistry.";		
+document.getElementById("det").innerHTML="Sir Ibtihaj is very calm and charming teacher,He has great past experince of teaching in different Educational InsitutesHe has passed his matriculation from Al- Hameed Secondary School ,Intermediate from Govt. Gulshan college , Graduation and masters degree from University of karachi.";		
+			
+}
+function babr()
+{
+document.getElementById("faimg").src="images/p3.jpg";	
+document.getElementById("name").innerHTML="Babar Manzar.";	
+document.getElementById("edu").innerHTML="BSSE Software Engineering."	;
+document.getElementById("exp").innerHTML="4 years of teaching Maths & Physics.";
+document.getElementById("ext").innerHTML="Physics & Maths and Computer";		
+document.getElementById("det").innerHTML="Sir Babar is a Software Engineer by professionally ,Along with computer skills He is excellent teacher of mathematics."	;	
+	
+}
+
+function salman()
+{
+document.getElementById("faimg").src="images/p4.jpg";	
+document.getElementById("name").innerHTML="Salman Masood";	
+document.getElementById("edu").innerHTML="MCS Computer Science"	;
+document.getElementById("exp").innerHTML="4 years of teaching Maths & O level cambridge system.";
+document.getElementById("ext").innerHTML="Maths and Computer";		
+document.getElementById("det").innerHTML="Sir Salman controls the Programs and I.T issues related to Educational Pearl Coaching Centre."	;	
+	
+	
+}
+
