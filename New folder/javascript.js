ae5d039b16076e9a8ccbc37d7039585aa2443921
salman <salmanmasood24@yahoo.com>
2016-06-25 19:12:50 +0500

    slider

--- New folder/javascript.js
+++ New folder/javascript.js
@@ -2,17 +2,18 @@ var temp=1;
 A=new Array("images/s1.jpg","images/s2.jpg","images/s3.png");
 function myfn()
 {	
+
 while(true)
 {
 switch(temp) {
 
-    case 0: document.getElementById("imgslider").src = A[0];
+    case 0: document.getElementById("sliderimg").src = A[0];
 	 temp = temp +1
 	 break;
-case 1: document.getElementById("imgslider").src = A[1];
+case 1: document.getElementById("sliderimg").src = A[1];
 	 temp = temp +1
 	 break;
-case 2: document.getElementById("imgslider").src = A[2];
+case 2: document.getElementById("sliderimg").src = A[2];
 	 temp = 0
 	 break;
 	 	  
